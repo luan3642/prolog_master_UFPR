852c2e0d1d5ee56814bac99e004b8e54afc5933f
luan3642 <luan3642@hotmail.com>
2024-03-16 14:05:41 -0300

    retomando primeiros passos prolog

---
+++ homem.pl
@@ -0,0 +1,18 @@
+% deixa os fatos de forma dinamica
+:- dynamic homem/1.
+
+%Adicionando e retirando fatos
+homem(jose).
+homem(mario).
+homem(alberto).
+homem(fabricio).
+
+% comando asserta insere um fato na base de dados, ele insere na primeira
+% posicao
+% asserta(homem(alex)).
+
+
+%assertz insere no final da base de dados
+% assertz(homem(luan)).
+
+%listing serve para lista todos da sua base de conhecimento
