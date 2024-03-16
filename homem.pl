830d72fa148f89f63d443c207127c7a6f3f93657
luan3642 <luan3642@hotmail.com>
2024-03-16 18:55:46 -0300

    ...

--- homem.pl
+++ homem.pl
@@ -2,14 +2,41 @@
 :- dynamic homem/1.
 
 %Adicionando e retirando fatos
+homem(joao).
 homem(jose).
 homem(mario).
 homem(alberto).
 homem(fabricio).
+homem(jonatas).
+homem(marcos).
 
 % comando asserta insere um fato na base de dados, ele insere na primeira
 % posicao
 % asserta(homem(alex)).
+
+%compondo fatos e regras
+mulher(claudia).
+mulher(silvia).
+mulher(clara).
+mulher(angela).
+mulher(maria).
+mulher(marcia).
+
+pai(joao, jose).
+pai(joao, maria).
+pai(silvia, jose).
+pai(silvia, maria).
+pai(alberto, joao).
+pai(fabricio, alberto).
+pai(jonatas, fabricio).
+
+%condicao para ver se é irmao
+irmao(X, Y):-
+    homem(X),
+    pai(Z, X),
+    pai(Z, Y),
+    mae(Z1, X),
+    mae(Z1, Y).
 
 
 %assertz insere no final da base de dados
