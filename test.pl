0547855c3ff62b2a6fe226ac0b90bab6e6842c46
luan3642 <luan3642@hotmail.com>
2024-03-21 15:18:44 -0300

    ...

--- test.pl
+++ test.pl
@@ -1,10 +1,18 @@
-homens([luan, marcos]).
+homens([luan,marcos,joao]).
 
-lista_dois_homens:-
-    homens(Homens),
-    length(Homens, Quantidade),
-    Quantidade =:= 2,
+% temos na nossa base de conhecimento os homens acima.
+
+%depois criamos uma regra
+
+lista_homens:-
+    homens(Homens), %aqui estamos atribuindo o valor da lista de homens para variavel Homens
+
+    % atribui o tamanho da variavel homens para variavel Quantos
+    length(Homens, Quantos),
+
+    % verifica se homens é igual a 2.
+    Quantos =:= 2,
+
+    % imprime homens.
     write(Homens), nl.
-
-
     
