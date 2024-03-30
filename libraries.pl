36561c0e28c82dcae07f3cc8aeb6762f555c5b18
luan3642 <luan3642@hotmail.com>
2024-03-30 11:11:13 -0300

    ...

--- libraries.pl
+++ libraries.pl
@@ -1,4 +1,7 @@
 book_title('The Hobbit', 'J.R.R. Tolkien').
+book_title('carros o filme', 'J.R.R. Tolkien').
+book_title('carros preto', 'J.R.R. Tolkien').
+book_title('carros azul', 'J.R.R. Tolkien').
 book_title('1984', 'George Orwell').
 book_title('Brave New World', 'Aldous Huxley').
 
@@ -6,7 +9,7 @@ author('J.R.R. Tolkien', 'fantasy').
 author('George Orwell', 'dystopian').
 author('Aldous Huxley', 'dystopian').
 
-searh_genre_book(X):-
+search_genre_book(X):-
     author(Y, X),
     book_title(Z, Y),
     write(Z).
